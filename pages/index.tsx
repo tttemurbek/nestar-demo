a279a27b19e004bde62c6e7b86276183d3b33746
tttemurbek <nonemenoneme9999@gmail.com>
2024-09-15 17:17:17 +0900

    feat: meta tags integrations on document private file

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,14 +27,14 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="https://nextjs.org/icons/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
+          <h1>
+            <div style={{ fontStyle: "italic", fontSize: "40px" }}>
+              I LOVE CODING
+            </div>
+            <div style={{ fontStyle: "italic", fontFamily: "monospace" }}>
+              MIT IS THE BEST
+            </div>{" "}
+          </h1>
           <ol>
             <li>
               Get started by editing <code>pages/index.tsx</code>.
